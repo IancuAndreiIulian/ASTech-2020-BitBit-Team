d0551ffd9da910e239fb340f43cfa233a82f9cab
iancu andrei <andrei-iulian.iancu@student.tuiasi.ro>
2022-03-17 10:38:44 +0200

    BitBit's workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/andy/catkin_ws/src
+# Build directory: /home/andy/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ackermann_vehicle/ackermann_vehicle")
+subdirs("ackermann_vehicle/ackermann_vehicle_description")
+subdirs("ackermann_vehicle/ackermann_vehicle_gazebo")
+subdirs("ackermann_msgs")
---
+++ build/ackermann_vehicle/ackermann_vehicle_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/andy/catkin_ws/src/ackermann_vehicle/ackermann_vehicle_description
+# Build directory: /home/andy/catkin_ws/build/ackermann_vehicle/ackermann_vehicle_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
